bb4e0ccea667e7b36966b40a469b35b657b35286
aludev <hackc7360@gmail.com>
2025-02-28 20:18:49 +0100

    create component topMenu

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,5 +1,11 @@
+import { TopMenu } from "@/components";
 import { ReactNode } from "react";
 
 export default function ShopLayout({ children }: { children: ReactNode }) {
-  return <main className="min-h-screen bg-red-500 ">{children}</main>;
+  return (
+    <main className="min-h-screen">
+      <TopMenu />
+      {children}
+    </main>
+  );
 }
