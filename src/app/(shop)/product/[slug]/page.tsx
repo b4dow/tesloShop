24ec5d10e657bfd20444d0f6ea82aab63e84e6f4
aludev <hackc7360@gmail.com>
2025-03-01 21:51:10 +0100

    slideshow mobile and implementation viewport

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,9 @@
-import { ProductSlideShow, QuantitySelector, SizeSelector } from "@/components";
+import {
+  ProductMobileSlideShow,
+  ProductSlideShow,
+  QuantitySelector,
+  SizeSelector,
+} from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
@@ -18,7 +23,18 @@ export default async function ProductPage({ params }: Props) {
     <div className="mt-5 pb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
       {/* Slideshow */}
       <div className="col-span-1 md:col-span-2 ">
-        <ProductSlideShow title={product.title} images={product.images} />
+        {/* Mobile Slideshow */}
+        <ProductMobileSlideShow
+          title={product.title}
+          images={product.images}
+          className="block md:hidden"
+        />
+        {/* Desktop Slideshow */}
+        <ProductSlideShow
+          title={product.title}
+          images={product.images}
+          className="hidden md:block"
+        />
       </div>
       {/* Detalles */}
       <div className="col-span-1 px-5 ">
