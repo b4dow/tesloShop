25a796ce98bfdc6c0dce08b5a7bd21a311cdb36f
aludev <hackc7360@gmail.com>
2025-03-04 00:24:13 +0100

    add component select order

--- src/app/(shop)/orders/[id]/page.tsx
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -1,7 +1,115 @@
-export default function OrderPage() {
+import { Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import { clsx } from "clsx";
+import Image from "next/image";
+import Link from "next/link";
+import { IoCardOutline } from "react-icons/io5";
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function OrderPage({ params }: Props) {
+  const { id } = params;
+
+  // TODO: verificar
+  //Redirect(/)
+
   return (
-    <div>
-      <h1>Order Page</h1>
+    <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+      <div className="flex flex-col w-[1000px] ">
+        <Title title={`Orden #${id}`} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/* Carrito */}
+          <div className="flex flex-col mt-5">
+            <div
+              className={clsx(
+                "flex items-center rounded-lg py-2 px-3.5 text-xl font-bold text-white mb-5",
+                {
+                  "bg-red-500": false,
+                  "bg-green-700": true,
+                },
+              )}
+            >
+              <IoCardOutline size={30} />
+              <span className="mx-2">Orden pagada</span>
+            </div>
+            {/* Items */}
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  alt={product.title}
+                  width={100}
+                  height={100}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                  className="mr-5 rounded"
+                  unoptimized
+                />
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price} x 3</p>
+                  <p className="font-bold">SubTotal ${product.price * 3}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* Checkout - Resumen de orden */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Dirección de entrega</h2>
+            <div className="mb-10">
+              <p className="text-2xl">Fernando Herrera</p>
+              <p>Av. siempre viva 123</p>
+              <p>Col. Centro</p>
+              <p>Alcadia Cuahtemoc</p>
+              <p>Ciudad de Mexico</p>
+              <p>CP 123123</p>
+              <p>123123123</p>
+            </div>
+            {/* Divider */}
+            <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+            <h2 className="text-2xl mb-2">Resumen de compra</h2>
+            <div className="grid grid-cols-2">
+              <span>No. Products</span>
+              <span className="text-right">3 articulos</span>
+
+              <span>SubTotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="text-2xl mt-5">Total</span>
+              <span className="mt-5 text-2xl text-right">$ 100</span>
+            </div>
+            <div className="mt-5 mb-2 w-full">
+              <div
+                className={clsx(
+                  "flex items-center rounded-lg py-2 px-3.5 text-xl font-bold text-white mb-5",
+                  {
+                    "bg-red-500": true,
+                    "bg-green-700": false,
+                  },
+                )}
+              >
+                <IoCardOutline size={30} />
+                <span className="mx-2">Pendiente</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
