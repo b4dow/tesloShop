f36fdc558d713b0387b5d8b5d4a1c094029bb843
aludev <hackc7360@gmail.com>
2025-03-04 16:10:06 +0100

    seed promise destroy model

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -3,11 +3,9 @@ import { initialData } from "./seed";
 
 async function main() {
   // 1. Borrar registros previos
-  await Promise.all([
-    await prisma.productImage.deleteMany(),
-    await prisma.product.deleteMany(),
-    await prisma.category.deleteMany(),
-  ]);
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
 
   const { categories, products } = initialData;
 
