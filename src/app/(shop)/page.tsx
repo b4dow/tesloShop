b0b8bfbee4931a5c30c29bc6764a8cbfbb1af87e
aludev <hackc7360@gmail.com>
2025-03-04 16:29:25 +0100

    get products of db

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,11 @@
+import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
 import { initialData } from "@/seed/seed";
 
 const products = initialData.products;
 
-export default function Shop() {
+export default async function Shop() {
+  const productsTemp = await getPaginatedProductsWithImages();
   return (
     <>
       <Title title="Tienda" subtitle="Todos los Productos" className="mb-2" />
