a6c408f6b70d4481b4c7287b5fbb219cf0463d0d
aludev <hackc7360@gmail.com>
2025-03-04 11:00:42 +0100

    relation product-category

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -16,6 +16,15 @@ async function main() {
   await prisma.category.createMany({
     data: categoriesData,
   });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce(
+    (map, category) => {
+      map[category.name.toLowerCase()] = category.id;
+      return map;
+    },
+    {} as Record<string, string>,
+  );
 }
 
 (() => {
