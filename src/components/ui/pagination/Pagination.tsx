e1b1f85a3794b3e32f37b45ebdb7d86c8abf06f3
aludev <hackc7360@gmail.com>
2025-03-05 01:09:12 +0100

    functionality pagination

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 import React from "react";
 import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 
@@ -7,15 +9,36 @@ interface Props {
 }
 
 export const Pagination = ({ totalPages }: Props) => {
-  console.log(totalPages);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) ?? 1;
+  console.log({ pathname, searchParams, currentPage });
+
+  const createPageUrl = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    if (pageNumber === "...") {
+      return `${pathname}?${params.toString()}`;
+    }
+
+    if (+pageNumber === 0) {
+      return `${pathname}`;
+    }
+
+    if (+pageNumber > totalPages) {
+      return `${pathname}?${params.toString()}`;
+    }
+
+    params.set("page", pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
   return (
     <div className="flex justify-center  text-center mt-10 mb-32">
       <nav aria-label="Page navigation example">
         <ul className="flex list-style-none items-center">
           <li className="page-item ">
             <Link
-              className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300  text-gray-800 pointer-events-none focus:shadow-none"
-              href="#"
+              className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300  text-gray-800  focus:shadow-none"
+              href={createPageUrl(currentPage - 1)}
             >
               <IoChevronBackOutline />
             </Link>
@@ -47,7 +70,7 @@ export const Pagination = ({ totalPages }: Props) => {
           <li className="page-item">
             <Link
               className="page-link relative block py-1.5 px-3 rounded border-0 bg-transparent outline-none transition-all duration-300  text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#"
+              href={createPageUrl(currentPage + 1)}
             >
               <IoChevronForwardOutline />
             </Link>
